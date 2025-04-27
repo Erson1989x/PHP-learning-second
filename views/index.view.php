952f8b7fb8e60a3aaa7790e7a60b49e85027e48e
Erson1989x <erson1989x@gmail.com>
2025-04-27 20:24:36 +0300

    progress

--- views/index.view.php
+++ views/index.view.php
@@ -1,14 +1,9 @@
 <?php require 'partials/head.php'; ?>
-<div class="min-h-full">
-    <?php require 'partials/nav.php'; ?>
-    <header class="bg-white shadow-sm">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p>This is the home page</p>
-        </div>
-    </main>
-    <?php require 'partials/footer.php'; ?>
+<?php require 'partials/nav.php'; ?>
+<?php require 'partials/banner.php'; ?>
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p>This is the home page</p>
+    </div>
+</main>
+<?php require 'partials/footer.php'; ?>
