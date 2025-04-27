ac3f98ebd65cd0d1521cd0d852092eb6045e71e5
Erson1989x <erson1989x@gmail.com>
2025-04-27 19:20:57 +0300

    progress

--- contact.view.php
+++ contact.view.php
@@ -121,12 +121,12 @@
 
         <header class="bg-white shadow-sm">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <p>This is the home page</p>
+                <p>This is the contact page</p>
             </div>
         </main>
     </div>
