8b85d4776a00b2d4a9426d73aac906588fb922ec
Erson1989x <erson1989x@gmail.com>
2025-04-27 20:05:08 +0300

    progress

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full">
-    <div class="min-h-full">
-    <?php require 'partials/nav.php'; ?>
-        <header class="bg-white shadow-sm">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <p>This is the contact page</p>
-            </div>
-        </main>
+<?php require 'partials/head.php'; ?>
+<?php require 'partials/nav.php'; ?>
+<header class="bg-white shadow-sm">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
     </div>
-
-</body>
-
-</html>
+</header>
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p>This is the contact page</p>
+    </div>
+</main>
+<?php require 'partials/footer.php'; ?>
