3202fb25986d0d0ccb2a7f6d132f4d861ba92000
Erson1989x <erson1989x@gmail.com>
2025-04-29 21:01:14 +0300

    progress

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
+<?php 
+
 require "functions.php";
-$heading = "Home";
-require "views/index.view.php";
+
+$URL = parse_url($_SERVER["REQUEST_URI"])["path"];
+
+if($URL === "/") {
+    require "controllers/index.php";
+} elseif($URL === "/about") {
+    require "controllers/about.php";
+} elseif($URL === "/contact") {
+    require "controllers/contact.php";
+}
+
+$routes = [
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/contact"=> "controllers/contact.php",
+];
+ 
