b00fb4da30d0962b4d91b700b8fd0034e91e48e5
Erson1989x <erson1989x@gmail.com>
2025-04-29 21:20:53 +0300

    progress

--- index.php
+++ index.php
@@ -2,19 +2,4 @@
 
 require "functions.php";
 
-$URL = parse_url($_SERVER["REQUEST_URI"])["path"];
-
-if($URL === "/") {
-    require "controllers/index.php";
-} elseif($URL === "/about") {
-    require "controllers/about.php";
-} elseif($URL === "/contact") {
-    require "controllers/contact.php";
-}
-
-$routes = [
-    "/" => "controllers/index.php",
-    "/about" => "controllers/about.php",
-    "/contact"=> "controllers/contact.php",
-];
- 
+require "router.php";
---
+++ router.php
@@ -0,0 +1,34 @@
+<?php
+
+$URL = parse_url($_SERVER["REQUEST_URI"])["path"];
+/*
+if($URL === "/") {
+    require "controllers/index.php";
+} elseif($URL === "/about") {
+    require "controllers/about.php";
+} elseif($URL === "/contact") {
+    require "controllers/contact.php";
+}
+    */
+
+$routes = [
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/contact"=> "controllers/contact.php",
+];
+
+function abourt($code = 404) {
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+
+function routeTo($URL, $routes) {
+    if(array_key_exists($URL, $routes)) {
+        require $routes[$URL];
+    } else {
+        abourt();
+    };
+}
+
+routeTo($URL, $routes);
