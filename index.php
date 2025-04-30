5bcd72aef9270ba6baf9841d7a436a519579a927
Erson1989x <erson1989x@gmail.com>
2025-04-30 22:24:45 +0300

    progress

--- index.php
+++ index.php
@@ -2,4 +2,32 @@
 
 require "functions.php";
 
-require "router.php";
+//require "router.php";
+
+// Connect to MySQL database
+/*
+class Person {
+    public $id; 
+    public $name;
+
+public function breathe() 
+    {
+        echo $this->name . " is breathing.";
+    }
+}
+
+$person = new Person();
+
+$person->id = 1;
+$person->name = "John Doe";
+
+dd($person ->breathe());
+*/
+$dsn = "mysql:host=localhost;port=3306;dbname=myfirstdb;charset=utf8mb4;user=root;password=";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM users");
+$statement->execute();
+$users = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($users);
