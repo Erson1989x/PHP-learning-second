4e46973aa95ff46fab08163dae89b6251590e9ab
Erson1989x <erson1989x@gmail.com>
2025-05-16 19:24:51 +0300

    progress

--- views/notes.view.php
+++ views/notes.view.php
@@ -4,15 +4,9 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <?php foreach ($notes as $note) : ?>
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4">
-                <div class="px-4 py-5 sm:px-6">
-                    <p class="mt-1 max-w-2xl text-sm text-gray-500"><?= $note['body'] ?></p>
-                </div>
-                <div class="border-t border-gray-200">
-                    <dl>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        </div>
-                    </dl>
+            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4 w-1/3">
+                <class="px-4 py-5 sm:px-6">
+                    <p class="mt-1 max-w-2xl text-sm text-gray-500"><a class="underline text-blue-500" href="note.php?id=<?= $note['id']; ?>"><?= $note['body']; ?></a>"</p>
                 </div>
             </div>
         <?php endforeach; ?>
