a110efe9cd81abec7b9bf9664a12a7a0e9f94489
Erson1989x <erson1989x@gmail.com>
2025-05-16 18:34:08 +0300

    progress

--- index.php
+++ index.php
@@ -2,22 +2,9 @@
 
 require "functions.php";
 
-//require "router.php";
+require "router.php";
 
 require "Database.php";
 $config = require "config.php";
 
-
-$db = new Database( $config['database'] );
-
-$id = $_GET['id'];
-
-$query = "SELECT * FROM users WHERE id = :id";
-
-$users = $db->query($query, [ ':id' => $id])->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($users as $user) {
-    echo "<li>" . $user['username'] . "</li>";
-}
-
-dd($users);
+$db = new Database($config['database']);
