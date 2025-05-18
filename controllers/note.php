d069d0a8737c610ac2dfd2fa95413c836d6f4fe6
Erson1989x <erson1989x@gmail.com>
2025-05-18 19:32:25 +0300

    fixes

--- controllers/note.php
+++ controllers/note.php
@@ -5,19 +5,17 @@ $config = require "config.php";
 $db = new Database($config['database']);
 
 $heading = "My Note";
+$currentUserId = 1; // Replace with the actual current user ID from your authentication system
 
 
 $note =  $db->query("SELECT * FROM notes where id = :id", [
     'id' => $_GET['id']
     ])->fetch(PDO::FETCH_ASSOC);
 
+
     if(!$note) {
         abourt();
-    }
-
-    $currentUserId = 1; // Replace with the actual current user ID from your authentication system
-
-    if(! $note['user_id'] !== $currentUserId) {
+    }    if($note['user_id'] !== $currentUserId) {
         abourt(Response::FORBIDDEN);
     }
 
--- views/notes.view.php
+++ views/notes.view.php
@@ -4,9 +4,8 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <?php foreach ($notes as $note) : ?>
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4 w-1/3">
-                <class="px-4 py-5 sm:px-6">
-                    <p class="mt-1 max-w-2xl text-sm text-gray-500"><a class="underline text-blue-500" href="note?id=1<?= $note['id']; ?>"><?= $note['body']; ?></a>"</p>
+            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4 w-1/3">                <div class="px-4 py-5 sm:px-6">
+                    <p class="mt-1 max-w-2xl text-sm text-gray-500"><a class="underline text-blue-500" href="note?id=<?= $note['id']; ?>"><?= $note['body']; ?></a></p>
                 </div>
             </div>
         <?php endforeach; ?>
